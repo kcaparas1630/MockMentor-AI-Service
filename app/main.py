fd4166a974c85978299681f20571c724368578d2
kcaparas1630 <kcaparas1630@gmail.com>
2025-05-15 16:44:17 -0700

    fixed imports

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from slowapi.errors import RateLimitExceeded
-from slowapi.util import _rate_limit_exceeded_handler
+from slowapi import _rate_limit_exceeded_handler
 from app.core.RouteLimiters import limiter
 from app.routes.health import router as health_router
 
