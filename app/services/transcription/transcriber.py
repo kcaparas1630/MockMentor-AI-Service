6ea8439fc05923973cb339594c0926e4cc66656e
kcaparas1630 <kcaparas1630@gmail.com>
2025-07-03 11:54:23 -0700

    removed setter and getter for audio. not needed anymore

--- app/services/transcription/transcriber.py
+++ app/services/transcription/transcriber.py
@@ -13,40 +13,45 @@ Authors: @kcaparas1630
 from faster_whisper import WhisperModel
 import tempfile
 import base64
+from loguru import logger
 
 _model = None
 
+class TranscriberService:
 
-def get_model():
-    """
-    Initializes and returns the WhisperModel instance.
-    This function ensures that the model is loaded only once and reused for subsequent calls.
-    
-    Returns:
-        WhisperModel: An instance of the WhisperModel configured for transcription.
-    """
-    global _model
-    if _model is None:
-        _model = WhisperModel("base", device="cpu", compute_type="int8")
-    return _model
+    def __init__(self):
+        self.model = self.get_model()
 
-def transcribe_base64_audio(base64_data: str) -> str:
-    """
-    Transcribes audio data encoded in base64 format to text.
-    Args:
-        base64_data (str): Base64 encoded audio data.
-    Returns:
+    def get_model(self):
+        """
+        Initializes and returns the WhisperModel instance.
+        This function ensures that the model is loaded only once and reused for subsequent calls.
+        
+        Returns:
+            WhisperModel: An instance of the WhisperModel configured for transcription.
+        """
+        global _model
+        if _model is None:
+            _model = WhisperModel("base", device="cpu", compute_type="int8")
+        return _model
+
+    def transcribe_base64_audio(self, base64_data: str) -> str:
+        """
+        Transcribes audio data encoded in base64 format to text.
+        Args:
+            base64_data (str): Base64 encoded audio data.
+        Returns:
         str: Transcribed text from the audio data.
     """
-    model = get_model()
-    # Decode to audio
-    audio_bytes = base64.b64decode(base64_data)
+        model = self.get_model()
+        # Decode to audio
+        audio_bytes = base64.b64decode(base64_data)
 
-    # Save to file
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".ogg") as temp_audio:
-        temp_audio.write(audio_bytes)
-        temp_path = temp_audio.name
-
-    # Transcribe using faster-whisper
-    segments, _ = model.transcribe(temp_path)
-    return " ".join([seg.text for seg in segments])
+        # Save to file
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".ogg") as temp_audio:
+            temp_audio.write(audio_bytes)
+            temp_path = temp_audio.name
+            
+        # Transcribe using faster-whisper
+        segments, _ = model.transcribe(temp_path)
+        return " ".join([seg.text for seg in segments])
